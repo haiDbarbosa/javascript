9d38733a6de50c218477c76ac5bd83613aef6116
haiDbarbosa <haianlobarbosa@gmail.com>
2023-10-31 18:40:32 -0400

    inicio ex05 e organização dos arquivos

---
+++ ex03/script.js
@@ -0,0 +1,6 @@
+    // var login = document.getElementById(email)
+    // var password = document.getElementById(password)
+    var n1 = Number(window.prompt ('Digite um numero'))
+    var n2 = Number(window.prompt ('Digite outro numero'))
+    var r = n1 + n2
+    window.alert(`A soma entre ${n1} e ${n2} é ` + String(r))
