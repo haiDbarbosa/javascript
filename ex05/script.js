1c781f24f15714a9d1b8b4d9b6e49145224c608c
haiDbarbosa <haianlobarbosa@gmail.com>
2023-10-31 19:36:22 -0400

    ex05

---
+++ ex05/script.js
@@ -0,0 +1,9 @@
+let corpo = document.getElementsByTagName('body')[0] ;
+let para = document.getElementsByTagName('h1')[0] ;
+let r = document.querySelector('div#msg') ;
+//   console.log(para)
+para.style.color = 'white' ;
+r.innerText = 'Olá mundo' ;
+corpo.style.background = 'grey'
+
+window.alert = ('olha só')
