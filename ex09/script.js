19ae279f0f345b8462961bd4b00ae11573b67751
haiDbarbosa <haianlobarbosa@gmail.com>
2024-01-07 19:40:08 -0400

    ex09 final

--- ex09/script.js
+++ ex09/script.js
@@ -2,21 +2,22 @@ function contar(){
     let start = document.getElementById ('inicio');
     let end = document.getElementById ('fim');
     let skip = document.getElementById ('gap');
-    let res = document.getElementById ('dois')
+    let res = document.getElementById ('dois');
 
     if(start.value.length == 0 || end.value.length == 0 || skip.value.length == 0) {
         window.alert('Preencha os dados corretamente')
     } else {
-        res.innerHTML = 'Contando...'
-        let i = Number(start.value);
-        let f = Number(end.value);
-        let s = Number(skip.value);
+        res.innerHTML = 'Contando...<br>'
+
+        let i = Number(start.value)
+        let f = Number(end.value)
+        let s = Number(skip.value)
 
         if (i < f) {
             for(let c = i; c <= f; c += s) {
             res.innerHTML += `${c} `
         }} else {
-            for(let c = i; c >= f; c += s) {
+            for(let c = i; c >= f; c -= s) {
             res.innerHTML += `${c} `
             }
         }
